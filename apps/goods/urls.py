f86f1f368fbbfc4e98242885a48c727577c69d71
dorothy <1815900164@qq.com>
2024-09-07 22:10:50 +0800

    提交

--- apps/goods/urls.py
+++ apps/goods/urls.py
@@ -1,7 +1,7 @@
-
+from apps.goods import views
 from django.contrib import admin
 from django.urls import path,include
 app_name='goods'
 urlpatterns = [#匹配url从上到下来的
-
+    path('',views.index,name='index'),#首页
 ]
--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,7 +1,8 @@
-
+from apps.user import views
 from django.contrib import admin
 from django.urls import path,include
 app_name='user'
 urlpatterns = [#匹配url从上到下来的
-
+    path("register/",views.register,name='register'),#注册
+    path("register_handle/",views.register_handle,name='register_handle'),#注册处理
 ]
