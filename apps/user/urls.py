8e963eefb80148e3be84602ce35271c75b953573
dorothy <1815900164@qq.com>
2024-09-10 15:10:23 +0800

    提交

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [#匹配url从上到下来的
     # path("register/",views.register,name='register'),#注册
     # path("register_handle/",views.register_handle,name='register_handle'),#注册处理
     path('register/',RegisterView.as_view(),name='register'),#注册
-    path('active/<str:token>/',ActiveView.as_view(),name='active'),#用户激活
+    path('active/<str:token>',ActiveView.as_view(),name='active'),#用户激活
     path('login/',LoginView.as_view(),name='login'),#登录
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.urls import reverse
 import re
 from apps.user.models import User
+from celery_tasks.tasks import send_register_active_email
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from itsdangerous import SignatureExpired
 from django.http import HttpResponse
@@ -114,25 +115,24 @@ class RegisterView(View):
         user = User.objects.create_user(username, email, password)
         user.is_active = 0  # 刚注册用户的时候它不应该是被激活的
         user.save()
+
         # 发送激活邮件，包括激活链接，http://172.0.0.1:8000/user/active/3
-        #激活链接中需要包含用户的身份信息，并且要对身份信息进行加密it's dangerous
+        #激活链接中需要包含用户的身份信息，并且要对身份信息进行加密
         #加密用户的身份信息，生成加密token
         serializer=Serializer(settings.SECRET_KEY,3600)
         info={'confirm':user.id}
         token=serializer.dumps(info)#bytes
         token=token.decode()
+        print(token)
         #发邮件
-        subject="天天生鲜欢迎信息"
-        message=''
-        html_message='<h1>%s,欢迎您成为天天生鲜注册会员</h1>请点击下面链接激活你的账户<br/><a href="http://127.0.0.1:8000/user/active/%s">http://127.0.0.1:8000/user/active/%s</a>'%(username,token,token)
-        sender =settings.EMAIL_FROM
-        receiver = [email]
-        send_mail(subject,message,sender,receiver,html_message=html_message)
+        send_register_active_email.delay(email,username,token)
+
 
 
         # 返回应答，跳转到首页
         return redirect(reverse('goods:index'))
 
+# /user/active/
 class ActiveView(View):
     def get(self,request,token):
         '''进行用户激活'''
@@ -140,8 +140,11 @@ class ActiveView(View):
         serializer = Serializer(settings.SECRET_KEY, 3600)
         try:
             info=serializer.loads(token)
+            # 打印出解码后的信息，方便调试
+            print(f"Decoded token info: {info}")
             #获取待激活用户的id
             user_id=info['confirm']
+            print(f"User ID from token: {user_id}")
             #根据id获取用户信息
             user=User.objects.get(id=user_id)
             user.is_active=1
